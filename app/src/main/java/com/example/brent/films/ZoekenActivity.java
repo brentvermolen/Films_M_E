2a314ecc3c48ccded8da1f20f3318a9aff01a100
Brent Vermolen <brentvermolen@hotmail.be>
2019-03-04 16:51:37 +0100

    ZoekenActivity

---
+++ app/src/main/java/com/example/brent/films/ZoekenActivity.java
@@ -0,0 +1,13 @@
+package com.example.brent.films;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZoekenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zoeken);
+    }
+}
