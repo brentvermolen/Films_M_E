8d1e6250332972a6d71e466ae8ed36117da2f479
Brent Vermolen <brentvermolen@hotmail.be>
2019-03-05 16:35:51 +0100

    back button genres

--- app/src/main/java/com/example/brent/films/GenresActivity.java
+++ app/src/main/java/com/example/brent/films/GenresActivity.java
@@ -1,7 +1,9 @@
 package com.example.brent.films;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
 
 import com.example.brent.films.Class.GenresAdapter;
@@ -9,11 +11,19 @@ import com.example.brent.films.Class.GenresAdapter;
 public class GenresActivity extends AppCompatActivity {
 
     ListView lstGenres;
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_genres);
+        setTitle("");
+
+        mToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        mToolbar.setBackgroundColor(Color.argb(100,255,255,255));
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         initViews();
         handleEvents();
@@ -26,5 +36,11 @@ public class GenresActivity extends AppCompatActivity {
 
     private void handleEvents() {
 
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
